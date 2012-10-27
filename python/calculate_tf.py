0c55c0ff8f34e28658cbdffb7534d8e55abc0112
Naoki Orii <mrorii@gmail.com>
2012-10-27 12:33:56 -0400

    Add python pygments code

---
+++ python/calculate_tf.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+import os
+import re
+import argparse
+import mimetypes
+from collections import defaultdict
+
+from pygments.lexers import guess_lexer_for_filename
+import pygments.token
+
+def filter_tokens(tokens):
+    return filter(lambda token: token[0] == pygments.token.Token.Name
+                          or token[0] == pygments.token.Token.Literal.String.Single
+                          or token[0] == pygments.token.Token.Literal.String
+                          or token[0] == pygments.token.Name.Constant
+                          or token[0] == pygments.token.Name.Function,
+                          tokens)
+
+def recursive_file_gen(directory):
+    for root, dirs, files in os.walk(directory):
+        for f in files:
+            yield os.path.join(root, f)
+
+class Tokenizer:
+    def __init__(self):
+        self.word_ptn      = re.compile(r'[A-Za-z]+')
+        self.first_cap_ptn = re.compile('(.)([A-Z][a-z]+)')
+        self.all_cap_ptn   = re.compile('([a-z0-9])([A-Z])')
+
+    def calculate_tf_for_directory(self, directory):
+        term_freq = defaultdict(int)
+
+        for f in recursive_file_gen(directory):
+            for token in self.get_tokens(f):
+                # preprocess tokens
+                tokens_preprocessed = self.preprocess(token)
+                for t in tokens_preprocessed:
+                    term_freq[t] += 1
+        return term_freq
+
+    def get_tokens(self, filepath):
+        code = open(filepath).read()
+        lexer = guess_lexer_for_filename(os.path.basename(filepath), code)
+
+        tokens = lexer.get_tokens(code)
+        tokens = filter_tokens(tokens)
+        return map(lambda token: token[1], tokens)
+
+    def preprocess(self, token):
+        retval = []
+
+        words = self.word_ptn.findall(token) # Remove/split on alphabetical characters
+
+        # Deal with CamelCase
+        for word in words:
+            s1 = self.first_cap_ptn.sub(r'\1 \2', word)
+            s2 = self.all_cap_ptn.sub(r'\1 \2', s1).lower()
+
+            s = s2.split(' ')
+            retval.extend(s)
+        return retval
+
+def main():
+    parser = argparse.ArgumentParser(description='Calculate term frequency')
+    parser.add_argument('--input', help='input directory', required=True)
+
+    args = parser.parse_args()
+
+    tokenizer = Tokenizer()
+    tf = tokenizer.calculate_tf_for_directory(args.input)
+    for term in sorted(tf):
+        freq = tf[term]
+        print '%s\t%s' % (term, freq)
+
+if __name__ == '__main__':
+    main()
